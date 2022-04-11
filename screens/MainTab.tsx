032318c1043047bf286fc6984b7cdf355c0d4b27
minataforest <minataforest@gmail.com>
2022-04-11 23:13:44 +0900

    220411 react-query 3

--- screens/MainTab.tsx
+++ screens/MainTab.tsx
@@ -3,19 +3,6 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {MainTabParamList} from './types';
 import ArticleScreen from './ArticleScreen';
 import MateralIcons from 'react-native-vector-icons/MaterialIcons';
-import {useQuery} from 'react-query';
-
-function Sample(id) {
-  const result = useQuery('articles', getArticles, {
-    enabled: true,
-    refetchOnMount: true,
-  });
-  const {data, error, isLoading} = result;
-
-  if (isLoading) return <Text>로딩 중</Text>;
-  if (error) return <Text>오류 발생</Text>;
-  return <View>{/* 데이터 보여주기 */}</View>;
-}
 
 const Tab = createBottomTabNavigator();
 

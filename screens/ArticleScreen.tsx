a2174299c04f8eeccd1626fbcbef83d10d30eb1d
minataforest <minataforest@gmail.com>
2022-04-15 23:25:56 +0900

    220415 react-query 7

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -2,21 +2,18 @@ import {RouteProp, useRoute} from '@react-navigation/core';
 import React from 'react';
 import {Text, View, ActivityIndicator, StyleSheet} from 'react-native';
 import {useQuery} from 'react-query';
-import {getArticles} from '../api/articles';
-import Articles from '../components/Articles';
+import {getArticle} from '../api/articles';
+import {getComments} from '../api/comments';
 import {RootStackParamList} from './types';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
 function ArticleScreen() {
   const {params} = useRoute<ArticleScreenRouteProp>();
-  //   const {data, isLoading} = useQuery('articles', getArticles);
+  const {id} = params;
 
-  //   if (!data) {
-  //     return <ActivityIndicator size="large" style={styles.spinner} />;
-  //   }
-  //   return <Articles articles={data} />;
-  // }
+  const articleQuery = useQuery(['articles', id], () => getArticle(id));
+  const commentsQuery = useQuery(['commetns', id], () => getComments(id));
 
   return (
     <View style={styles.block}>
